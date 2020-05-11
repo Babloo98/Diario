88cdb8d55adb3007ce8bb63db27dcd2ba372b5bd
Joyesh <joyesh.malik@tothenew.com>
2020-05-11 18:20:05 +0530

    Dashboard ui Done

--- src/components/Dashboard/Home/index.js
+++ src/components/Dashboard/Home/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import StarRatings from 'react-star-ratings';
+import { HtmlEditor, MenuBar } from '@aeaton/react-prosemirror'
+import { options, menu } from '@aeaton/react-prosemirror-config-default'
 import './style.scss';
 
 class Home extends React.Component{
@@ -15,6 +17,10 @@ class Home extends React.Component{
         this.setState({
           rating: newRating
         });
+    }
+
+    onChange = () => {
+
     }
 
     render(){
@@ -38,7 +44,20 @@ class Home extends React.Component{
                     </div>
                 </div>
                 <div className="input-wrap"><input type="date" className="diary-date" /></div>
-                <div className="diary-content"><textarea placeholder="ENTER YOU THOUGHTS"></textarea></div>
+                <div className="diary-content">
+                    {/* <textarea placeholder="ENTER YOU THOUGHTS"></textarea> */}
+                    <HtmlEditor
+                        options={options}
+                        value={''}
+                        onChange={this.onChange}
+                        render={({ editor, view }) => (
+                        <div>
+                            <MenuBar menu={menu} view={view} />
+                            {editor}
+                        </div>
+                        )}
+                    />
+                </div>
             </div>
         )
     }
