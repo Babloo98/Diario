b417d711431a4031d2d64ac94354e27226582f45
Joyesh <joyesh.malik@tothenew.com>
2020-05-11 13:53:19 +0530

    Star rating on dashboard introduced

--- src/components/Dashboard/Home/index.js
+++ src/components/Dashboard/Home/index.js
@@ -1,11 +1,42 @@
 import React from "react";
+import StarRatings from 'react-star-ratings';
 import './style.scss';
 
 class Home extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            rating: 0
+        };
+    };
+
+    changeRating = ( newRating, name ) => {
+        this.setState({
+          rating: newRating
+        });
+    }
+
     render(){
         return(
             <div className="dashboard">
-                <div className="input-wrap"><input placeholder="ENTER TITLE HERE" /></div>
+                <div className="input-wrap">
+                    <input placeholder="ENTER TITLE HERE" />
+                    <div className="diary-rating">
+                        <p>How was your day</p>
+                        <StarRatings
+                            rating={this.state.rating}
+                            starRatedColor="blue"
+                            changeRating={this.changeRating}
+                            numberOfStars={5}
+                            name='rating'
+                            starDimension={'20px'}
+                            starEmptyColor={'gray'}
+                            starRatedColor={'#fcba03'}
+                            starHoverColor={'rgb(218,165,32)'}
+                        />
+                    </div>
+                </div>
                 <div className="input-wrap"><input type="date" className="diary-date" /></div>
                 <div className="diary-content"><textarea placeholder="ENTER YOU THOUGHTS"></textarea></div>
             </div>
